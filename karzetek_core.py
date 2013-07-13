9a2945002226315f770ff970ed7f4b517408734e
Reuben Castelino <projectdelphai@gmail.com>
2013-07-13 00:37:53 -0700

    refactoring core

--- karzetek_core.py
+++ karzetek_core.py
@@ -1,56 +1,47 @@
 #! /usr/bin/python
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import urllib.request
 import re
 import feedparser
+import time
 
 class Karzetek:
   def url_recommendations(self,url):
     rss_feeds=[]
-    for link in self.get_hyperlinks(url):
+    links = self.get_hyperlinks(url)
+    for link in links:
       rss_feed = self.get_rss(link)
       if not rss_feed == None:
         rss_feeds.append(rss_feed)
     return rss_feeds
  
   def feed_recommendations(self,rss_link):
+    print(time.ctime())
     feed = feedparser.parse(rss_link)
     recommendations=[]
     for entry in feed.entries[:1]:
       recommendations = recommendations + self.url_recommendations(entry.link)
+    print(time.ctime())
     return recommendations
   
   def get_hyperlinks(self,url):
-    html = urllib.request.urlopen(url)
-    soup = BeautifulSoup(html, "lxml")
+    soup = BeautifulSoup(urllib.request.urlopen(url), "lxml", parse_only=SoupStrainer('a'))
     links=[]
     base_url = re.match('(http|https)://[^/]*', url).group(0)
-    for a in soup.findAll('a'):
-      try:
-        link = a["href"]
-      except:
-        continue
-      if not link.startswith(base_url) and link.startswith('http'):
-        links.append(link)
-    links = self.filter_for_dups(links)
+    for a in soup.findAll('a', href=True):
+      if not a['href'].startswith(base_url) and a['href'].startswith('http'):
+        link_base_url = re.match('(http|https)://[^/]*', a['href']).group(0)
+        if link_base_url not in links:
+          links.append(link_base_url)
     return links
-  
-  def filter_for_dups(self,array):
-    filtered_array=[]
-    for element in array:
-      base_url = re.match('(http|https)://[^/]*', element).group(0)
-      if base_url not in filtered_array:
-        filtered_array.append(base_url)
-    return filtered_array
 
   def get_rss(self,url):
     try:
-      html = urllib.request.urlopen(url, timeout = 10)
+      html = urllib.request.urlopen(url, timeout = 1)
     except:
       return
-    soup = BeautifulSoup(html, "lxml")
-    for a in soup.findAll('link'):
-      if a.has_attr('rel') and a.has_attr('type') and a.has_attr('href'):
-        if a['rel'] == ['alternate']:
-          return { "title": ' '.join(soup.title.text.split()).encode('utf-8'), "site": url, "feed": a['href'] }
+    soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer('head'))
+    for a in soup.findAll('link', {'rel' : 'alternate'}, href=True, type=True ):
+      if a['rel'] == ['alternate'] and a['href'].startswith('http'):
+        return { "title": ' '.join(soup.title.text.split()).encode('utf-8'), "site": url, "feed": a['href'] }
