c38e1286cc75f88db83342eec4a1ce73461bdd9c
Reuben Castelino <projectdelphai@gmail.com>
2013-07-24 02:12:40 -0700

    rename files for readability

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 karzetek:app
+web: gunicorn -w 4 index:app
--- karzetek.py
+++ karzetek.py
@@ -1,20 +1,47 @@
 #! /usr/bin/python
 
-from flask import Flask
-from flask import request
-from karzetek_core import Karzetek
+from bs4 import BeautifulSoup, SoupStrainer
+import urllib.request
+import re
+import feedparser
+import time
 
-app = Flask(__name__)
+class Karzetek:
+  def url_recommendations(self,url):
+    rss_feeds=[]
+    links = self.get_hyperlinks(url)
+    for link in links:
+      rss_feed = self.get_rss(link)
+      if not rss_feed == None:
+        rss_feeds.append(rss_feed)
+    return rss_feeds
+ 
+  def feed_recommendations(self,rss_link):
+    print(time.ctime())
+    feed = feedparser.parse(rss_link)
+    recommendations=[]
+    for entry in feed.entries[:1]:
+      recommendations = recommendations + self.url_recommendations(entry.link)
+    print(time.ctime())
+    return recommendations
+  
+  def get_hyperlinks(self,url):
+    soup = BeautifulSoup(urllib.request.urlopen(url), "lxml", parse_only=SoupStrainer('a'))
+    links=[]
+    base_url = re.match('(http|https)://[^/]*', url).group(0)
+    for a in soup.findAll('a', href=True):
+      if not a['href'].startswith(base_url) and a['href'].startswith('http'):
+        link_base_url = re.match('(http|https)://[^/]*', a['href']).group(0)
+        if link_base_url not in links:
+          links.append(link_base_url)
+    return links
 
-@app.route('/')
-def index():
-  input = request.args.get('feed', '')
-  if len(input) < 1:
-    return "No input"
-  else:
-    recommended_feeds = Karzetek().feed_recommendations(input)
-    return str(recommended_feeds)
-
-if __name__ == '__main__':
-  app.debug = True
-  app.run()
+  def get_rss(self,url):
+    try:
+      html = urllib.request.urlopen(url, timeout = 1)
+    except:
+      return
+    soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer('head'))
+    for a in soup.findAll('link', {'rel' : 'alternate'}, href=True, type=True ):
+      if a['rel'] == ['alternate'] and a['href'].startswith('http'):
+        return { "title": ' '.join(soup.title.text.split()).encode('utf-8'), "site": url, "feed": a['href'] }
